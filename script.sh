a1501bbe22a79d38e846e343061d5a9954117255
SupriyaMutyala <55025383+SupriyaMutyala@users.noreply.github.com>
2020-08-19 16:52:47 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@ set -xe
 sudo apt-get update
 sudo apt-get -y install ansible
 sudo apt-get -y install git
-git clone 
-cd ansible-play; sudo ansible-playbook main.yml
+git clone https://github.com/SupriyaMutyala/finaltask.git
+cd finaltask; sudo ansible-playbook kubernates.yml
