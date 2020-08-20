f02aead85294f5ddaea678c8836ec63eed538342
SupriyaMutyala <55025383+SupriyaMutyala@users.noreply.github.com>
2020-08-19 20:13:30 -0400

    Update seed.groovy

--- x/seed.groovy
+++ x/seed.groovy
@@ -1,1 +1,14 @@
-folder("test_folder")
+path = "finaltask/x"
+
+pipelineJob(path) {
+    description("This job creates a jumphost to access jenkins agents run in Openstack VMs")
+    logRotator {
+        daysToKeep(30)
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace("${path}/Jenkinsfile.groovy"))
+            sandbox(false)
+        }
+    }
+}
