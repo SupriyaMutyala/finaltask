5173406834ce3200154b3d3251bc25a5d675e0fb
SupriyaMutyala <55025383+SupriyaMutyala@users.noreply.github.com>
2020-08-26 12:06:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,108 +1,56 @@
-provider "azurerm" {
-  version = "= 2.0.0"
-  subscription_id = "c568e566-0087-4834-a7bc-58890077b576"
-  client_id       = "2621181f-da45-4ee8-82c1-748894f1971d"
-  client_secret   = "xAPaN5XPjwcFhJfa0o5-64M-.qgt6zbKOL"
-  tenant_id       = "643b5e21-4adc-4b59-bf35-18321f054c84"
+provider "google" {
+credentials = file("key.json")
+project = "eternal-unity-287515"
+region = "us-central1"
+zone  = "us-central1-a"
 
-  features {}
 }
 
-variable "prefix" {
-  default = "tfvmex"
-}
+resource "google_compute_instance" "default" {
+  name         = "virtual-machine-from-terraform"
+  machine_type = "f1-micro"
 
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
-  location = "West US 2"
-}
-
-resource "azurerm_public_ip" "example" {
-  name                    = "test-pip"
-  location                = azurerm_resource_group.main.location
-  resource_group_name     = azurerm_resource_group.main.name
-  allocation_method       = "Dynamic"
-  idle_timeout_in_minutes = 25
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix       = "10.0.2.0/24"
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-resource "azurerm_virtual_machine" "main" {
- name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-}
-resource "azurerm_virtual_machine_extension" "example" {
-  name                 = "hostname"
-  virtual_machine_id   = azurerm_virtual_machine.main.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-  settings = <<SETTINGS
-    {
-      "fileUris": ["https://raw.githubusercontent.com/SupriyaMutyala/finaltask/master/script.sh"],
-      "commandToExecute": "bash script.sh"
-
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
     }
-SETTINGS
-  tags = {
-    environment = "Production"
   }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+    // Apply the firewall rule to allow external IPs to access this instance
+    tags = ["http-server"]
 }
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_network" "default" {
+  name = "test-network"
+}
+
+output "ip" {
+  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+}
+
