231655e68701f78798b9293c7f897875c70198f4
SupriyaMutyala <55025383+SupriyaMutyala@users.noreply.github.com>
2020-08-26 12:14:30 -0400

    Update seed.groovy

--- x/seed.groovy
+++ x/seed.groovy
@@ -1,6 +1,6 @@
-base_path = "finaltask"
+base_path = "finaltask1"
 folder("${base_path}")
-job_path = "${base_path}/x"
+job_path = "${base_path}/y"
 
 pipelineJob(job_path) {
     description("This job creates a jumphost to access jenkins agents run in Openstack VMs")
